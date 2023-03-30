bfae149614e8e2d39ad879960564e8ed112bae5a
Alex Rozanski <alex@rozanski.me>
2023-03-30 11:05:26 +0100

    add hover state to source type row

--- camel/ui/setup/SourceTypeSelectionView.swift
+++ camel/ui/setup/SourceTypeSelectionView.swift
@@ -7,35 +7,45 @@
 
 import SwiftUI
 
+struct SourceTypeSelectionRow: View {
+  let source: SourceTypeSelectionViewModel.Source
+
+  @State var isHovered = false
+
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading, spacing: 4) {
+        Text(source.name)
+          .fontWeight(.bold)
+        Text(source.description)
+      }
+      .padding()
+      Spacer()
+      Image(systemName: "chevron.right")
+        .padding(.trailing)
+    }
+    .background(isHovered ? Color("SourceTypeSelectionRowHover") : .clear)
+    .onHover { isHovered = $0 }
+  }
+}
+
 struct SourceTypeSelectionView: View {
   var viewModel: SourceTypeSelectionViewModel
 
   var body: some View {
-    VStack(alignment: .leading) {
+    VStack(alignment: .leading, spacing: 0) {
       ForEach(viewModel.sources, id: \.id) { source in
-        HStack {
-          VStack(alignment: .leading, spacing: 4) {
-            Text(source.name)
-              .fontWeight(.bold)
-            Text(source.description)
-          }
-          .padding()
-          Spacer()
-          Image(systemName: "chevron.right")
-            .padding(.trailing)
-        }
+        SourceTypeSelectionRow(source: source)
         if source.id != viewModel.sources.last?.id {
           Divider()
         }
       }
     }
-    .background(
+    .mask(RoundedRectangle(cornerRadius: 4))
+    .background(Color(cgColor: NSColor.systemGray.withAlphaComponent(0.05).cgColor))
+    .overlay(
       RoundedRectangle(cornerRadius: 4)
-        .fill(Color(cgColor: NSColor.systemGray.withAlphaComponent(0.05).cgColor))
-        .overlay(
-          RoundedRectangle(cornerRadius: 4)
-            .stroke(Color(cgColor: NSColor.separatorColor.cgColor))
-        )
+        .stroke(Color(cgColor: NSColor.separatorColor.cgColor))
     )
   }
 }
