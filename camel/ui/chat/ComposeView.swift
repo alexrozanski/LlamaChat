cf5a87b12c77b2c44fac6012de10b2183f54c110
Alex Rozanski <alex@rozanski.me>
2023-03-31 19:54:19 +0100

    spruce up send button

--- camel/ui/chat/ComposeView.swift
+++ camel/ui/chat/ComposeView.swift
@@ -13,25 +13,36 @@ struct ComposeView: View {
   @FocusState private var isFocused: Bool
 
   var body: some View {
-    HStack {
+    let canSendMessage = !viewModel.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    HStack(spacing: 4) {
       BorderlessTextField("Chat here...", text: $viewModel.text)
         .focused($isFocused)
-        .padding(.horizontal, 10)
         .padding(.vertical, 4)
-        .background(
-          RoundedRectangle(cornerRadius: 15)
-            .fill(.white)
-            .overlay {
-              RoundedRectangle(cornerRadius: 15)
-                .stroke(Color(NSColor.separatorColor.cgColor))
-            }
-        )
-      Button(action: {
-        viewModel.send(message: viewModel.text)
-      }, label: {
-        Text("Send")
-      })
+      if canSendMessage {
+        Button(action: {
+          viewModel.send(message: viewModel.text)
+        }, label: {
+          Image(systemName: "arrow.up")
+            .padding(3)
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Circle())
+        })
+        .buttonStyle(BorderlessButtonStyle())
+        .keyboardShortcut(.return, modifiers: [])
+      }
     }
+    .padding(.vertical, 2)
+    .padding(.leading, 10)
+    .padding(.trailing, 5)
+    .background(
+      RoundedRectangle(cornerRadius: 15)
+        .fill(.white)
+        .overlay {
+          RoundedRectangle(cornerRadius: 15)
+            .stroke(Color(NSColor.separatorColor.cgColor))
+        }
+    )
     .padding()
     .onAppear {
       isFocused = true
