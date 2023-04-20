8fc01b98b4c190de23b8a56aad11a375d69733c7
Alex Rozanski <alex@rozanski.me>
2023-04-20 15:15:48 +0100

    add model params to chat info view

--- LlamaChat/ui/chat/ChatInfoView.swift
+++ LlamaChat/ui/chat/ChatInfoView.swift
@@ -48,58 +48,137 @@ struct ChatInfoView: View {
 
   @State private var showClearMessagesAlert = false
 
-  var body: some View {
-    Form {
-      Section {
-        VStack {
-          AvatarView(viewModel: viewModel.avatarViewModel, size: .large)
-            .padding(.bottom, 8)
-          VStack(spacing: 4) {
-            Text(viewModel.name)
-              .font(.headline)
-            Text(viewModel.modelType)
-              .font(.system(size: 12))
-          }
+  @ViewBuilder var header: some View {
+    Section {
+      VStack {
+        AvatarView(viewModel: viewModel.avatarViewModel, size: .large)
+          .padding(.bottom, 8)
+        VStack(spacing: 4) {
+          Text(viewModel.name)
+            .font(.headline)
+          Text(viewModel.modelType)
+            .font(.system(size: 12))
         }
-        .frame(maxWidth: .infinity)
       }
-      Section {
-        HStack(spacing: 16) {
-          ActionButton(title: "clear", imageName: "trash.circle.fill", enabledTextColor: .red, handler: {
-            showClearMessagesAlert = true
-          })
-          .disabled(!viewModel.canClearMessages)
-          ActionButton(title: "info", imageName: "info.circle.fill", enabledTextColor: .blue, handler: {
-            SettingsWindowPresenter.shared.present(deeplinkingTo: .sources(sourceId: viewModel.sourceId))
-          })
-        }
-        .frame(maxWidth: .infinity, alignment: .center)
-      }
-      Section {
-        LabeledContent(content: {
-          Text(viewModel.modelSize)
-        }, label: {
-          Text("Model Size")
+      .frame(maxWidth: .infinity)
+    }
+  }
+
+  @ViewBuilder var actions: some View {
+    Section {
+      HStack(spacing: 16) {
+        ActionButton(title: "clear", imageName: "trash.circle.fill", enabledTextColor: .red, handler: {
+          showClearMessagesAlert = true
+        })
+        .disabled(!viewModel.canClearMessages)
+        ActionButton(title: "info", imageName: "info.circle.fill", enabledTextColor: .blue, handler: {
+          SettingsWindowPresenter.shared.present(deeplinkingTo: .sources(sourceId: viewModel.sourceId))
         })
       }
-      Section(content: {
-        LabeledContent(content: {
-          VStack(alignment: .trailing) {
-            modelStatText(modelStat: viewModel.contextTokenCount, unit: Unit(singular: "token", plural: "tokens"))
-            if let value = viewModel.contextTokenCount.value, value > 0 {
-              Button(action: {
-                openWindow(id: WindowIdentifier.modelContext.rawValue, value: viewModel.sourceId)
-              }, label: { Text("Show...") })
-                .focusable(false)
-            }
+      .frame(maxWidth: .infinity, alignment: .center)
+    }
+  }
+
+  @ViewBuilder var properties: some View {
+    Section {
+      LabeledContent(content: {
+        Text(viewModel.modelSize)
+      }, label: {
+        Text("Model Size")
+      })
+    }
+  }
+
+  @ViewBuilder var parameters: some View {
+    Section(content: {
+      LabeledContent {
+        Text(viewModel.seedValue.map { "\($0)" } ?? "Randomized")
+      } label: {
+        Text("Seed")
+      }
+      LabeledContent {
+        Text("\(viewModel.contextSize) tokens")
+      } label: {
+        Text("Context Size")
+      }
+      LabeledContent(content: {
+        VStack(alignment: .trailing) {
+          modelStatText(modelStat: viewModel.contextTokenCount, unit: Unit(singular: "token", plural: "tokens"))
+          if let value = viewModel.contextTokenCount.value, value > 0 {
+            Button(action: {
+              openWindow(id: WindowIdentifier.modelContext.rawValue, value: viewModel.sourceId)
+            }, label: { Text("Show...") })
+            .focusable(false)
           }
-        }, label: {
-          Text("Current Context")
-        })
-      }, header: {
+        }
+      }, label: {
+        Text("Current Context")
+      })
+      LabeledContent {
+        Text("\(viewModel.numberOfTokens) tokens")
+      } label: {
+        Text("Max Response")
+      }
+    }, header: {
+      HStack {
         Text("Model Parameters")
           .font(.system(.body).smallCaps())
-      })
+        Spacer()
+        Button("Configure...") {}
+          .font(.system(size: 11))
+          .controlSize(.small)
+      }
+    })
+
+    Section {
+      LabeledContent {
+        Text("\(viewModel.topP, specifier: "%.2f")")
+          .fontDesign(.monospaced)
+      } label: {
+        Text("Top-p")
+      }
+      LabeledContent {
+        Text("\(viewModel.topK)")
+          .fontDesign(.monospaced)
+      } label: {
+        Text("Top-k")
+      }
+      LabeledContent {
+        Text("\(viewModel.temperature, specifier: "%.2f")")
+          .fontDesign(.monospaced)
+      } label: {
+        Text("Temperature")
+      }
+      LabeledContent {
+        Text("\(viewModel.batchSize)")
+          .fontDesign(.monospaced)
+      } label: {
+        Text("Batch Size")
+      }
+    }
+
+    Section {
+      LabeledContent {
+        Text("\(viewModel.repeatPenalty, specifier: "%.2f")")
+          .fontDesign(.monospaced)
+      } label: {
+        Text("Repeat Penalty")
+      }
+      LabeledContent {
+        Text("\(viewModel.lastNTokensToPenalize)")
+          .fontDesign(.monospaced)
+      } label: {
+        Text("Penalization Window")
+      }
+    }
+  }
+
+  var body: some View {
+    Form {
+      header
+      actions
+      properties
+      parameters
     }
     .formStyle(.grouped)
     .frame(width: 280)
